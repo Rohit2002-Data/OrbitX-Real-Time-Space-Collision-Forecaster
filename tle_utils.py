98228c70d71db60a811ab0632ade092808614636
Rohit2002-Data <rohitkumargitam021@gmail.com>
2025-06-26 13:47:40 +0530

    Update tle_utils.py

--- tle_utils.py
+++ tle_utils.py
@@ -6,7 +6,6 @@ import pandas as pd
 TLE_SOURCES = {
     "All Active Satellites": "https://celestrak.org/NORAD/elements/gp.php?GROUP=active&FORMAT=tle",
     "Starlink":           "https://celestrak.org/NORAD/elements/gp.php?GROUP=starlink&FORMAT=tle",
-    "GPS-OPS": "https://celestrak.org/NORAD/elements/gp.php?GROUP=gps-ops&FORMAT=tle",
     "Iridium":            "https://celestrak.org/NORAD/elements/gp.php?GROUP=iridium&FORMAT=tle"
 }
 
